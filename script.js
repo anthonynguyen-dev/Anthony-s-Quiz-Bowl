74a05ee807a718895b376f61ba784b2b3f2bfbfa
anthonynguyen-dev <nguyen.dev@protonmail.com>
2022-11-02 22:54:47 -0500

    added functions to generate answers and questions in script.js as well as more btns in index.html

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 var startBtn = document.querySelector("#start-btn");
+var answerA = document.querySelector("#answer-a");
+var answerB = document.querySelector("#answer-b");
+var answerC = document.querySelector("#answer-c");
+var answerD = document.querySelector("#answer-d");
+var answerBtns = document.querySelectorAll(".answer-btn");
+var questionHolder = document.querySelector("#question-container");
+var question = document.querySelector("#question");
+var currentQuestion = 0;
+
 var questions = [
   {
     questionText: "What kind of casing is most commonly used in Javascript?",
     answerChoices: ["LowerCase", "UpperCase", "CamelCase", "PascalCase"],
     correctAnswer: "CamelCase",
   },
+  {
+    questionText:
+      "What is the correct syntax to add an event listener to a button?",
+    answerChoices: [
+      "element.addEventListener",
+      "addEventListener.element",
+      "eventListenerElement.add",
+      "ListenerEventElement.add",
+    ],
+    correctAnswer: "element.addEventListener",
+  },
 ];
 function startQuiz() {
-  console.log("hello");
+  question.textContent = questions[currentQuestion].questionText;
+  answerA.textContent = questions[currentQuestion].answerChoices[0];
+  answerB.textContent = questions[currentQuestion].answerChoices[1];
+  answerC.textContent = questions[currentQuestion].answerChoices[2];
+  answerD.textContent = questions[currentQuestion].answerChoices[3];
 }
+function answerCheck(event) {
+  if (event.target.textContent === questions[currentQuestion].correctAnswer) {
+    console.log("correct");
+  } else {
+    console.log("incorrect");
+  }
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    startQuiz();
+  }
+}
+function endQuiz() {}
+
 startBtn.addEventListener("click", startQuiz);
+
+for (let i = 0; i < answerBtns.length; i++) {
+  answerBtns[i].addEventListener("click", answerCheck);
+}
